201207395a5d73b5e826c3bfbee1b04ad232af4b
bragceo <119948453+bragceo@users.noreply.github.com>
2023-05-14 12:34:40 -0700

    Set-up additional model files and index.js file to import the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {onDelete: "CASCADE"}); 
 
 // Categories have many Products
+Category.hasMany(Product); 
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag, onDelete: 'CASCADE' });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, onDelete: 'CASCADE' });
 
 module.exports = {
   Product,
@@ -18,3 +22,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
